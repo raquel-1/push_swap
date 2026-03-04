78a4cd90753e48c4f9a1ea897dd2e66399877a1a
raqroca- <raqroca-@student.42madrid.com>
2026-03-04 18:38:42 +0100

    error fied again

--- utils_sort_complex.c
+++ utils_sort_complex.c
@@ -6,7 +6,7 @@
 /*   By: raqroca- <raqroca-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/04 11:35:12 by raqroca-          #+#    #+#             */
-/*   Updated: 2026/03/04 18:00:58 by raqroca-         ###   ########.fr       */
+/*   Updated: 2026/03/04 18:32:40 by raqroca-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,20 @@
 ** If the 2º is the max, rotate it backwards and check if it needs to be swapped.
 ** In any other case, just swap the first two.
 
-** Sort 4 elements (Recursive Extraction):
-** 1. Finds the element with pos=0.
-** 2. Calculates if it's faster to bring it to the top via ra or rra.
-** 3. Pushes it to B, sorts the remaining 3 with sort_three_by_pos,
-** and finally pushes the 0 back to A.
+** Sort 4 elements (Minimum Extraction):
+** 1. Identifies the position of the smallest element (pos=0).
+** 2. Moves it to the top of Stack A using the shortest path (ra/rra).
+** 3. Pushes it to Stack B, sorts the remaining 3 with sort_three_by_pos,
+** and finally returns the 0 to the top of A.
 
-** Sort 5 elements (Modular Extraction):
-** 1. Locates the smallest element (pos=0).
-** 2. Uses the shortest path (ra/rra) to move it to the top and pushes to B.
-** 3. Calls sort_four_by_pos to handle the remaining 4 elements.
-** 4. Pushes the 0 back from B to A to complete the sorted stack.
+** Sort 5 elements (Dual Minimum Extraction):
+** 1. Finds the smallest element (pos=0) and moves it to the top (ra/rra) 
+** before pushing it to Stack B.
+** 2. Repeats the process for the next smallest element (pos=1) and
+** pushes it to Stack B as well.
+** 3. Sorts the 3 remaining elements in A with sort_three_by_pos.
+** 4. Returns both elements from B to A (pos 1, then pos 0) to complete 
+** the ascending order.
 */
 
 void	sort_two_by_pos(t_stack **stack, int print)
@@ -68,8 +71,21 @@ void	sort_three_by_pos(t_stack **stack, int print)
 
 void	sort_four_by_pos(t_stack **stack, int print)
 {
-	while ((*stack)->stacka->pos != 0)
-		ra(stack, print);
+	int	size;
+	int	index;
+
+	size = ft_lstsize((*stack)->stacka);
+	index = get_pos_index((*stack)->stacka, 0);
+	if (index <= size / 2)
+	{
+		while ((*stack)->stacka->pos != 0)
+			ra(stack, print);
+	}
+	else
+	{
+		while ((*stack)->stacka->pos != 0)
+			rra(stack, print);
+	}
 	pb(stack, print);
 	sort_three_by_pos(stack, print);
 	pa(stack, print);
@@ -77,10 +93,29 @@ void	sort_four_by_pos(t_stack **stack, int print)
 
 void	sort_five_by_pos(t_stack **stack, int print)
 {
-	while ((*stack)->stacka->pos != 0)
-		ra(stack, print);
+	int	size;
+	int	index;
+
+	size = ft_lstsize((*stack)->stacka);
+	index = get_pos_index((*stack)->stacka, 0);
+	if (index <= size / 2)
+		while ((*stack)->stacka->pos != 0)
+			ra(stack, print);
+	else
+		while ((*stack)->stacka->pos != 0)
+			rra(stack, print);
 	pb(stack, print);
-	sort_four_by_pos(stack, print);
+	size = ft_lstsize((*stack)->stacka);
+	index = get_pos_index((*stack)->stacka, 1);
+	if (index <= size / 2)
+		while ((*stack)->stacka->pos != 1)
+			ra(stack, print);
+	else
+		while ((*stack)->stacka->pos != 1)
+			rra(stack, print);
+	pb(stack, print);
+	sort_three_by_pos(stack, print);
+	pa(stack, print);
 	pa(stack, print);
 }
 
